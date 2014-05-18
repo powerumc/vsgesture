420653b3e830f52ead1de8047eb6a12569a717ed
땡초(엄준일) <umc@coupang.com>
2014-05-18 20:59:16 +0900

    seperate in index.html

---
+++ js/markdown-toc.js
@@ -0,0 +1,42 @@
+/**
+ * User: powerumc
+ * Date: 2014. 5. 18.
+ * Time: 오후 8:39
+ */
+
+
+$(function() {
+	$(function () {
+		$.ajax({
+			url: "README.md",
+			type: "GET",
+			success: function (data) {
+				var html = marked(data);
+				$("#preview").html(html);
+
+				var toc = $("#toc").tocify({    context: "#preview",
+					selectors: "h1, h2, h3",
+					showEffect: "slideDown",
+					hideEffect: "slideUp",
+					theme: "bootstrap",
+					hashGenerator: "pretty",
+					highlightOffset: 40});
+
+
+				$("code").addClass("prettyprint");
+
+				prettyPrint();
+
+				$(document).ready(function () {
+					$('[data-toggle=offcanvas]').click(function () {
+						$('.row-offcanvas').toggleClass('active');
+					});
+				});
+
+				$("#title").text($("#preview h1:first").text());
+			}
+		});
+	});
+
+});
+
